08ccf4286dc0ddd568a6217c238a080c7063106e
tanmay pakhira <tanmaypc001@gmail.com>
2025-11-25 16:13:03 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+
 import authRoutes from './routes/auth.route.js';
 import mesaageRoutes from './routes/message.route.js';
 
@@ -7,13 +9,22 @@ dotenv.config();
 
 
 const app = express();
-
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", mesaageRoutes);
+
+//make ready for production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
