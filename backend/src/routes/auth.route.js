b7138698a2eaf0d01688cc54b18f1ac04274a96d
tanmay pakhira <tanmaypc001@gmail.com>
2025-11-28 16:34:13 +0530

    sidebar completed

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -19,7 +19,7 @@ router.post("/login", login);
 
 router.post("/logout", logout);
 
-router.post("/update-profile", protectRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
 
 router.get("/check", protectRoute, (req, res) => res.status(200).json(req.user));
 
